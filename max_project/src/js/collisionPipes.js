6407c05b4f7b007821d8a3c9079d4b983c7c3318
bagmanmax <73840379+bagmanmax@users.noreply.github.com>
2020-12-15 13:18:07 +0100

    Update collisionPipes.js

--- max_project/src/js/collisionPipes.js
+++ max_project/src/js/collisionPipes.js
@@ -9,26 +9,38 @@ tPipe.src = "public/images/topPipe.png";
 ;
 var icons = new Image();
 icons.src = "public/images/bang3.png";
+var pipeSpawn = 200;
 
 class Pipes {
-  constructor() {
+  constructor() { 
     this.topPipe = (Math.random() * canvas.height) / 3 + 15; // top of the screen
+    this.bottomPipe += 15;
+    this.topCollision=  250-this.topPipe;// collision for the top pipe 
     this.bottomPipe = (Math.random() * canvas.height) / 3 + 15; // bottom of the screen
+    this.bottomPipe -= 15;
+    this.bottomCollision= canvas.height - this.bottomPipe;
+
     this.x = canvas.width;
     this.width = 93;
     this.height = 250;
     this.keepScore = false; // we use this to augment the score in the update method
   }
   draw() {
-    ctx.drawImage(tPipe, this.x, -this.topPipe, this.width, this.height);// drawing the top pipe
+    
+    
+     ctx.drawImage(tPipe, this.x, -this.topPipe, this.width, this.height);// drawing the top pipe
 
-    ctx.drawImage(          //drawing the bottom pipe
-      bPipe,
-      this.x,
-      canvas.height - this.bottomPipe,
-      this.width,
-      this.height
-    );
+     ctx.drawImage(          //drawing the bottom pipe
+       bPipe,
+       this.x,
+       canvas.height - this.bottomPipe,
+       this.width,
+       this.height
+     );
+    /* ctx.fillStyle="#ff000099";
+     ctx.fillRect(this.x, -this.topPipe, this.width, this.height);
+     ctx.fillRect(this.x, canvas.height - this.bottomPipe, this.width, this.height);*/
+
   }
 
   update() {
@@ -41,9 +53,8 @@ class Pipes {
     }
   }
 }
-
 function createPipes() {
-  if (frame % 80 === 0) {
+  if (frame % pipeSpawn === 0) {
     pipesArray.push(new Pipes()); // this adds a new pipe to the array with the constructor every 80 frames
   }
   for (let i = 0; i < pipesArray.length; i++) {
